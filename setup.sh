392d8db03ffe7b70c2368aed11df5f35a1254316
SWS Tech Dev Team <dev@sws-international.com>
2019-12-06 06:26:22 -0800

    sudo need not be used in root env. (For other env, all commands should be run with sudo)

--- setup.sh
+++ setup.sh
@@ -35,9 +35,8 @@ wget -O /tmp/pkglist.json https://raw.githubusercontent.com/swstechdev/freenas/m
 iocage create -r 11.2-RELEASE -p /tmp/pkglist.json -n storjadmin allow_raw_sockets=1 defaultrouter="192.168.1.1" resolver="nameserver 192.168.1.1; nameserver 8.8.8.8"  ip4_addr="lo${lanid}|192.168.0.${lanid}/24"
 
 wget -O /tmp/storjadmin.json  https://raw.githubusercontent.com/swstechdev/freenas/master/storj.json 
-#iocage fetch -P dhcp=on vnet=on bpf=yes -n /tmp/storjadmin.json --branch 'master'
-iocage fetch -P -n /tmp/storjadmin.json --branch 'master'
+#iocage fetch -P -n /tmp/storjadmin.json --branch 'master' # Not required
 
 iocage exec storjadmin git clone https://github.com/swstechdev/freenas.git /root/storjadmin
-sudo iocage exec storjadmin bash /root/storjadmin/post_install.sh standard
+iocage exec storjadmin bash /root/storjadmin/post_install.sh standard
 
