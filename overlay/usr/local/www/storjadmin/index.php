ff210abe5d84452916b41f59ab9ce5ac5bf4858e
SWS Tech Dev Team <dev@sws-international.com>
2019-12-06 06:22:37 -0800

    Launch of shell script validated in storj jail env
     Changes to be committed:
    	modified:   overlay/usr/local/www/storjadmin/index.php
    	new file:   overlay/usr/local/www/storjadmin/sample.sh

--- overlay/usr/local/www/storjadmin/index.php
+++ overlay/usr/local/www/storjadmin/index.php
@@ -1,6 +1,6 @@
 <?php 
-
-echo "Place holder for Storj Admin base index file !! <br>" ; 
+$out = shell_exec("bash sample.sh fileA file2 fileCDE 2>&1 ");
+echo "Sample Shell run : <br>" . $out ;
 phpinfo();
 
 ?>
